6d872b1ce60ce1d3c35606c9a0a2f9a5ded280d1
Genghis Bai <bai.genghis@gmail.com>
2018-06-16 13:45:45 -0700

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel5
+                    Laravel 5
                 </div>
 
                 <div class="links">
